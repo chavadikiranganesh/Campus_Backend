74946dd109d0badbf098644a7dcd809ff89203ea
chavadikiranganesh <chavadikiranganesh@gmail.com>
2026-03-12 10:39:47 +0530

    Add direct MongoDB connection to index.js

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 require('dotenv').config()
+const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
-const connectDB = require('./db')
 const { User, StudyMaterial, Accommodation, LostFound, Event, StudyGroup, LoginLog, Notification } = require('./models')
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error:', err))
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
-
-// Connect to MongoDB
-connectDB()
 
 async function isAdmin(userId) {
   try {
